335edef59743f97fb00bf5b6556e2d22a86b72dc
Machajdík Lukáš <machajdik@cngroup.dk>
2022-03-17 18:16:38 +0100

    added latitude and longitude parameters to map

--- TeamFinder/Client/wwwroot/bing-map.js
+++ TeamFinder/Client/wwwroot/bing-map.js
@@ -1,5 +1,7 @@
-﻿function loadBingMap() {
-    var map = new Microsoft.Maps.Map(document.getElementById('map'), {});
+﻿function loadBingMap(latitude, longitude) {
+    var map = new Microsoft.Maps.Map(document.getElementById('map'), {
+        center: new Microsoft.Maps.Location(latitude, longitude),
+    });
     var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), null);
     map.entities.push(pushpin);
     return "";
