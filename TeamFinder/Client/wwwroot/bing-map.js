1dbc800129ebe9354cef049b014b5ab8578664a8
Machajdík Lukáš <machajdik@cngroup.dk>
2022-03-23 21:00:59 +0100

    works in details too

--- TeamFinder/Client/wwwroot/bing-map.js
+++ TeamFinder/Client/wwwroot/bing-map.js
@@ -1,7 +1,7 @@
-﻿function initMap(long,lat)
+﻿function initMap(long,lat, id)
 {
     var center = SMap.Coords.fromWGS84(long, lat);
-    var map = new SMap(JAK.gel('map'), center, 12);
+    var map = new SMap(JAK.gel(String(id)), center, 12);
     map.addDefaultLayer(SMap.DEF_BASE).enable();
     map.addDefaultControls();
 
@@ -16,5 +16,16 @@
 
 function simpleMap(long,lat,uniqueId)
 {
-    
+    var center = SMap.Coords.fromWGS84(long, lat);
+    var map = new SMap(JAK.gel(String(uniqueId)), center, 13);
+    map.addDefaultLayer(SMap.DEF_BASE).enable();
+
+    var layer = new SMap.Layer.Marker();
+    map.addLayer(layer);
+    layer.enable();
+
+    var options = {};
+    var marker = new SMap.Marker(center, "myMarker", options);
+    layer.addMarker(marker);
+
 }
